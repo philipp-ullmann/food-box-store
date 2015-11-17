99202dfb9304001fbd2af93f75b48e9035bbdd14
Philipp Ullmann <ullmann.philipp@gmail.com>
2015-11-17 07:01:32 +0100

    Aligned order form

--- src/food/box/views/order.clj
+++ src/food/box/views/order.clj
@@ -33,66 +33,64 @@
                 :terms-accepted
                 :box])])
 
-      [:form.pure-form.pure-form-stacked {:method "post" :action "/order"}
+      [:form.pure-form.pure-form-aligned {:method "post" :action "/order"}
         [:fieldset
-          [:legend "Address"]
-      
-          [:div.pure-g
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "first-name"} "First Name *"]
-              [:input#first-name.pure-u-23-24 {:type     "text"
-                                               :name     "order[first-name]"
-                                               :value    first-name
-                                               :required true}]]
-      
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "last-name"} "Last Name *"]
-              [:input#last-name.pure-u-23-24 {:type     "text"
-                                              :name     "order[last-name]"
-                                              :value    last-name
-                                              :required true}]]
-      
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "email"} "Email Address *"]
-              [:input#email.pure-u-23-24 {:type     "email"
-                                          :name     "order[email]"
-                                          :value    email
-                                          :required true}]]
-      
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "street"} "Street *"]
-              [:input#street.pure-u-23-24 {:type     "text"
-                                           :name     "order[street]"
-                                           :value    street
-                                           :required true}]]
-
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "postcode"} "Postcode / Zip *"]
-              [:input#postcode.pure-u-23-24 {:type     "text"
-                                             :name     "order[postcode]"
-                                             :value    postcode
-                                             :required true}]]
-
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "city"} "Town / City *"]
-              [:input#city.pure-u-23-24 {:type     "text"
-                                         :name     "order[city]"
-                                         :value    city
-                                         :required true}]]
-
-            [:div.pure-u-1.pure-u-md-1-3
-              [:label {:for "country"} "Country *"]
-              [:select#country.pure-input-1-2 {:name "order[country]"}
-                                              (country-options country)]]]
-      
-          [:label.pure-checkbox {:for "terms-accepted"}
-            [:input#terms-accepted {:type    "checkbox"
-                                    :name    "order[terms-accepted]"
-                                    :checked terms-accepted}
-                                   " I've read the terms and conditions"]]
-
           [:input#box {:type  "hidden"
                        :name  "order[box]"
                        :value box}]
+
+          [:div.pure-control-group
+            [:label {:for "first-name"} "First Name *"]
+            [:input#first-name {:type     "text"
+                                :name     "order[first-name]"
+                                :value    first-name
+                                :required true}]]
       
-          [:button.pure-button.pure-button-primary {:type "submit"} "Confirm"]]]]))
+          [:div.pure-control-group
+            [:label {:for "last-name"} "Last Name *"]
+            [:input#last-name {:type     "text"
+                               :name     "order[last-name]"
+                               :value    last-name
+                               :required true}]]
+      
+          [:div.pure-control-group
+            [:label {:for "email"} "Email Address *"]
+            [:input#email {:type     "email"
+                           :name     "order[email]"
+                           :value    email
+                           :required true}]]
+      
+          [:div.pure-control-group
+            [:label {:for "street"} "Street *"]
+            [:input#street {:type     "text"
+                            :name     "order[street]"
+                            :value    street
+                            :required true}]]
+
+          [:div.pure-control-group
+            [:label {:for "postcode"} "Postcode / Zip *"]
+            [:input#postcode {:type     "text"
+                              :name     "order[postcode]"
+                              :value    postcode
+                              :required true}]]
+
+          [:div.pure-control-group
+            [:label {:for "city"} "Town / City *"]
+            [:input#city {:type     "text"
+                          :name     "order[city]"
+                          :value    city
+                          :required true}]]
+
+          [:div.pure-control-group
+            [:label {:for "country"} "Country *"]
+            [:select#country {:name "order[country]"}
+                             (country-options country)]]
+      
+          [:div.pure-control-group
+            [:label.pure-checkbox {:for "terms-accepted"}
+              [:input#terms-accepted {:type    "checkbox"
+                                      :name    "order[terms-accepted]"
+                                      :checked terms-accepted}
+                                     " I've read the terms and conditions"]]
+      
+            [:button.pure-button.pure-button-primary {:type "submit"} "Confirm"]]]]]))
