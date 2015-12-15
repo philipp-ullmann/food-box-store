0052969de7913b39a86f02e8e5a90684a42f10d0
Philipp Ullmann <ullmann.philipp@gmail.com>
2015-12-15 06:50:10 +0100

    Removed logo header item

--- src/food/box/views/application.clj
+++ src/food/box/views/application.clj
@@ -31,11 +31,10 @@
 
     [:body
       [:div.pure-menu.pure-menu-horizontal
-        [:a.pure-menu-heading {:href "#"} "LOGO"]
         [:ul.pure-menu-list
-          [:li.pure-menu-item.pure-menu-selected [:a.pure-menu-link {:href "/"} "TYPES OF BOXES"]]
-          [:li.pure-menu-item [:a.pure-menu-link {:href "/contact"}             "CONTACT"]]
-          [:li.pure-menu-item [:a.pure-menu-link {:href "#"}                    "ABOUT US"]]]]
+          [:li.pure-menu-item [:a.pure-menu-link {:href "/"}        "TYPES OF BOXES"]]
+          [:li.pure-menu-item [:a.pure-menu-link {:href "/contact"} "CONTACT"]]
+          [:li.pure-menu-item [:a.pure-menu-link {:href "#"}        "ABOUT US"]]]]
 
       (if banner? [:div.banner [:hi.banner-head "SIMPLE, UNIQUE" [:br] "AND TASTY!"]])
 
